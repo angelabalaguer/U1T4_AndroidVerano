f34efd6f53992458ea5493c0ec56a50639d70fc3
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-01-16 19:42:07 -0500

    implementacion

---
+++ app/src/main/java/com/example/u1t4_androidverano/ServicioMusica.java
@@ -0,0 +1,82 @@
+package com.example.u1t4_androidverano;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+public class ServicioMusica extends Service {
+    MediaPlayer reproductor;
+
+    private static final int ID_NOTIFICACION_CREAR = 1;
+    public static final String NOTIFICATION_CHANNEL_ID = "1000";
+    public static final String NOTIFICATION_CHANNEL_NAME = "UNJBG";
+
+    @Override public void onCreate() {
+        Toast.makeText(this,"Servicio creado",
+                Toast.LENGTH_SHORT).show();
+        reproductor = MediaPlayer.create(this, R.raw.musica);
+    }
+    @Override
+    public int  onStartCommand(Intent intenc, int flags, int idArranque) {
+        NotificationCompat.Builder notific = new NotificationCompat.Builder(this)
+                .setContentTitle("Creando Servicio de Música")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("información adicional")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                        android.R.drawable.ic_media_play))
+                .setWhen(System.currentTimeMillis() + 1000 * 60 * 60)
+                .setContentInfo("más info")
+                .setTicker("Texto en barra de estado")
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setDefaults(Notification.DEFAULT_VIBRATE);;
+
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(
+                            NOTIFICATION_CHANNEL_ID,
+                            NOTIFICATION_CHANNEL_NAME,
+                            NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(R.color.colorAccent);
+            notificationManager.createNotificationChannel(notificationChannel);
+            notific.setChannelId(NOTIFICATION_CHANNEL_ID);
+
+            PendingIntent intencionPendiente = PendingIntent.getActivity(
+                    this, 0, new Intent(this, Servicio.class), 0);
+            notific.setContentIntent(intencionPendiente);
+        }
+        notificationManager.notify(ID_NOTIFICACION_CREAR, notific.build());
+        Toast.makeText(this,"Servicio arrancado "+ idArranque,
+                Toast.LENGTH_SHORT).show();
+        reproductor.start();
+        return START_STICKY;
+
+    }
+
+    @Override public void onDestroy() {
+        Toast.makeText(this,"Servicio detenido",
+                Toast.LENGTH_SHORT).show();
+        reproductor.stop();
+//Eliminando la notificacion
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(ID_NOTIFICACION_CREAR);
+
+    }
+    @Override public IBinder onBind(Intent intencion) {
+        return null;
+    }
+}
